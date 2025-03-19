35bdc24178b59049390d79fd75a4dad7465187f3
Bhoomika <gargbhoomika79@gmail.com>
2025-03-19 14:54:53 +0530

    Deployement

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -91,6 +91,6 @@ export const logout = (req, res) => {
 		res.status(200).json({ message: "Logged out successfully" });
 	} catch (error) {
 		console.log("Error in logout controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
+		res.status(500).json({ error: "Internal Error" });
 	}
 };
